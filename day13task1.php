b573d8b7309eb4fae526cd7015b9b5d3f9ac3cfc
AWLR <lehrelex@gmail.com>
2023-12-16 12:19:06 +0800

    Create php file

---
+++ day13task1.php
@@ -0,0 +1,14 @@
+<?php
+
+$age = 76;
+
+if($age >= 25 && $age <= 65 ){
+    echo "you are allow to drive";
+}elseif($age > 65){
+    echo "you are not allow to drive";
+}else{
+    echo "you are not allow to drive";
+}
+
+
+?>
